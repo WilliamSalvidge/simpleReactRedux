77688b875edb6dd01f05cae5d37ab4380360d1cd
William Salvidge <willsalvidge@googlemail.com>
2021-10-26 15:46:24 +0100

    added Link and Button from libraries

--- app/components/ResultsRedux.js
+++ app/components/ResultsRedux.js
@@ -3,7 +3,8 @@ import React, {
   useEffect,
   useContext
 } from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 import Loading from "./Loading";
 import { useSelector } from "react-redux";
 
